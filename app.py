678b3313405d566d3f215f4469c73946aa57d079
vinit765 <chaudharyvinit700@gmail.com>
2025-02-28 23:10:11 +0530

    Resolved merge conflicts and updated files

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import joblib
 app = Flask(__name__)
 CORS(app)
 
-# Load trained model, scaler, and label encoder
+
 model = joblib.load("student_grade_model.pkl")
 scaler = joblib.load("scaler.pkl")
 label_encoder = joblib.load("label_encoder.pkl")
@@ -16,7 +16,7 @@ def predict():
     data = request.json
 
     try:
-        # Convert input data into array format with correct column names
+
         features = np.array([
             [
                 data['Attendance (%)'],
@@ -29,13 +29,13 @@ def predict():
             ]
         ])
 
-        # Apply the same normalization as in training
+        
         features_scaled = scaler.transform(features)
 
-        # Make prediction (Get the grade index)
+        
         grade_index = model.predict(features_scaled)[0]
 
-        # Convert the numeric grade back to the letter grade (A, B, C, etc.)
+        
         predicted_grade = label_encoder.inverse_transform([grade_index])[0]
 
         return jsonify({"predicted_grade": predicted_grade})
